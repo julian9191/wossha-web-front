26a46c7f6a0e63e292a883d259658a22da944df3
julian <juliancho9191@gmail.com>
2018-10-25 18:27:11 -0500

    Se modifica apariencia inicial del calendario

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -6,7 +6,8 @@ declare var $: any;
 @Component({
     moduleId: module.id,
     selector: 'calendar-cmp',
-    templateUrl: 'calendar.component.html'
+	templateUrl: 'calendar.component.html',
+	styleUrls: [ 'calendar.component.css' ]
 })
 
 export class CalendarComponent implements OnInit{
@@ -16,7 +17,15 @@ export class CalendarComponent implements OnInit{
         var today = new Date();
         var y = today.getFullYear();
         var m = today.getMonth();
-        var d = today.getDate();
+		var d = today.getDate();
+		
+		var popTemplate = [
+			'<div class="popover" style="max-width:400px;" >',
+				'<div class="popover-header">',
+					'<h3 class="popover-title"></h3>',
+				'</div>',
+				'<div class="popover-content"></div><br>',
+			'</div>'].join('');
 
         $calendar.fullCalendar({
             viewRender: function(view, element) {
@@ -28,7 +37,7 @@ export class CalendarComponent implements OnInit{
             },
             header: {
 				left: 'title',
-				center: 'month,agendaWeek,agendaDay',
+				//center: 'month,agendaWeek,agendaDay',
 				right: 'prev,next,today'
 			},
 			defaultDate: today,
@@ -80,35 +89,65 @@ export class CalendarComponent implements OnInit{
             // color classes: [ event-blue | event-azure | event-green | event-orange | event-red ]
             events: [
 				{
-					title: 'All Day Event',
+					title: 'Camiseta verde Americanino',
 					start: new Date(y, m, 1),
-                    className: 'event-default'
+					backgroundColor: "#7aee25",
+					allDay: true,
+					imageurl: this.getImage('75bcf6d1-d700-11e8-bdff-55e7a7a77f37'),
+					description: "esto es una prendita"
+				},
+				{
+					title: 'Camiseta verde Americanino',
+					start: new Date(y, m, 1),
+					className: 'event-orange',
+					allDay: true,
+					imageurl:this.getImage('75bcf6d1-d700-11e8-bdff-55e7a7a77f37'),
+					description: "esto es una prendita"
+				},
+				{
+					title: 'Camiseta verde Americanino',
+					start: new Date(y, m, 1),
+					className: 'event-orange',
+					allDay: true,
+					imageurl:this.getImage('75bcf6d1-d700-11e8-bdff-55e7a7a77f37'),
+					description: "esto es una prendita"
 				},
 				{
 					id: 999,
-					title: 'Repeating Event',
-					start: new Date(y, m, d-4, 6, 0),
-					allDay: false,
-					className: 'event-rose'
+					start: new Date(y, m, 2),
+					title  : 'event',
+					className: 'event-orange',
+					allDay: true,
+        			imageurl:this.getImage('75bcf6d1-d700-11e8-bdff-55e7a7a77f37'),
+					description: "esto es una prendita"
+				},
+				{
+					id: 999,
+					start: new Date(y, m, 2),
+					title  : 'event',
+					className: 'event-orange',
+					allDay: true,
+        			imageurl:this.getImage('75bcf6d1-d700-11e8-bdff-55e7a7a77f37'),
+					description: "esto es una prendita"
 				},
 				{
 					id: 999,
 					title: 'Repeating Event',
 					start: new Date(y, m, d+3, 6, 0),
-					allDay: false,
+					allDay: true,
 					className: 'event-rose'
 				},
 				{
 					title: 'Meeting',
 					start: new Date(y, m, d-1, 10, 30),
-					allDay: false,
+					allDay: true,
 					className: 'event-green'
 				},
 				{
 					title: 'Lunch',
 					start: new Date(y, m, d+7, 12, 0),
 					end: new Date(y, m, d+7, 14, 0),
-					allDay: false,
+					allDay: true,
 					className: 'event-red'
 				},
 				{
@@ -121,24 +160,57 @@ export class CalendarComponent implements OnInit{
 					title: 'Birthday Party',
 					start: new Date(y, m, d+1, 19, 0),
 					end: new Date(y, m, d+1, 22, 30),
-					allDay: false,
+					allDay: true,
                     className: 'event-azure'
 				},
 				{
 					title: 'Click for Wossha',
 					start: new Date(y, m, 21),
-					end: new Date(y, m, 22),
+					allDay: true,
 					url: 'https://www.wossha.com/',
 					className: 'event-orange'
 				},
 				{
 					title: 'Click for Google',
 					start: new Date(y, m, 21),
-					end: new Date(y, m, 22),
+					allDay: true,
 					url: 'https://www.wossha.com/',
 					className: 'event-orange'
 				}
-			]
+			],
+			eventRender: function(event, eventElement) {
+				let pictureUrl = event.imageurl ? event.imageurl : "../assets/img/blog-1.jpg";
+				eventElement.find("div.fc-content").prepend(`<img src='${pictureUrl}' width='20%'>`);
+				eventElement.popover({
+					title: event.title,
+					content: function () {
+						return `<div class='col-md-12' style='padding: 0 0 10px 0'>
+									<div class='col-md-6'  style='padding: 0'>
+										<img style='width:100%' src='${event.imageurl}' />
+									</div>
+									<div class='col-md-6'>
+										<p style='font-size: 9pt'>${event.description}</p>
+									</div>
+								</div>`;
+					},
+					template: popTemplate,
+					html: true,
+					trigger: 'hover'/*'click'*/,
+					placement: 'bottom',
+					animation: 'true',
+					container: 'body'
+				});
+			},
 		});
-    }
+
+	}
+
+	getImage(uuid:string):string{
+		console.log(uuid);
+		if(uuid){
+			return "http://localhost:8083/pictures/static-picture/"+uuid;
+		}else{
+			return "../assets/img/blog-1.jpg";
+		}
+	}
 }
